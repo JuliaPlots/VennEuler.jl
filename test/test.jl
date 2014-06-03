ad8381bdef7099c39276de1dc055562b20df8d14
Harlan Harris <harlan@harris.name>
2014-06-03 15:06:27 -0400

    test improvements

--- test/test.jl
+++ test/test.jl
@@ -1,6 +1,8 @@
 using VennEuler, Base.Test
 
 # just use @test macro
+# run with:
+# reload("src/VennEuler.jl"); include("test/test.jl")
 
 srand(1)
 randdata = randbool(20, 3) # 3 cols
@@ -8,11 +10,12 @@ setlabels = ["A", "B", "C"]
 
 # convert the random data into a DisjointSet
 ds1 = DisjointSet(randdata, setlabels)
-@show ds1
+@test ds1.counts == [4,3,1,3,3,1,1,4]
 
 # make an Euler object
-eo = makeeulerobject(setlabels, vec(sum(randdata,1)), ds1)
-@show eo
+es, eo = makeeulerobject(setlabels, vec(sum(randdata,1)), ds1)
+@test_approx_eq_eps(eo.lb, [0.155172,0.155172,0.155172,0.155172,0.189655,0.189655], .0001)
+@test all(eo.lb .<= es .<= eo.ub)
 
 # tests for bitmap operations
 bmc1 = VennEuler.makebitmapcircle(.5, .5, .2, 20)
