8c543b3154eadeeca7c437054c2703d49a19f6c9
Harlan Harris <harlan@harris.name>
2014-06-13 20:42:35 -0400

    clamp DSDC in 2nd DC2 test

--- test/DC2.jl
+++ test/DC2.jl
@@ -11,7 +11,9 @@ println("got $minf at $minx (returned $ret)")
 
 render("test/DC2.svg", eo, minx)
 
-eo = make_euler_object(labels, data, [EulerSpec(:circle), EulerSpec(:square), EulerSpec(:triangle),
+# do it again, but with different shapes, and with DSDC (the 2nd one) clamped in the center
+eo = make_euler_object(labels, data, [EulerSpec(:circle), EulerSpec(:square, [.5, .5], [0, 0]), 
+	EulerSpec(:triangle),
 	EulerSpec(:circle), EulerSpec(:square), EulerSpec(:triangle)]) # mixed shapes
 
 (minf,minx,ret) = optimize(eo, random_state(eo), ftol=-1, xtol=0.0025, maxtime=240, pop=1000)
